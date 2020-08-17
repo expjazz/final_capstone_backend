26c0a93264da7a1773a108872701fbc603369aad
Expedito Andrade <expeditojazz@gmail.com>
2020-08-17 06:34:02 -0300

    add post request to registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,10 @@
 class RegistrationsController < Devise::SessionsController
   respond_to :json
+  def create
+    @user = User.new(user_params)
+    @user.profile = Candidate.new(candidate_params)
+    @user.save
+  end
 
   private
 
@@ -10,4 +15,12 @@ class RegistrationsController < Devise::SessionsController
   def respond_to_on_destroy
     head :ok
       end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def candidate_params
+    params.require(:candidate).permit(:name)
+  end
 end
