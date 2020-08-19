7a895bed5db2bf353a43d0b47e8aa598059c506f
Expedito Andrade <expeditojazz@gmail.com>
2020-08-19 14:03:34 -0300

    add company details create backend

--- app/controllers/company_details_controller.rb
+++ app/controllers/company_details_controller.rb
@@ -3,9 +3,9 @@ class CompanyDetailsController < ApplicationController
 
   def create
     # @curriculum = Curriculum.new
-    token = info_current_user
-    user = User.find(token[0]['user_id'])
-    id = Company.find(user.profile_id)
+    @user = User.new(user_params)
+    @company = Company.new(company_params)
+    @user.profile = @company
     address = CompanyAddress.create(params_address)
     personal = CompanyPersonal.create(params_personal)
     @company = Company.find(id)
@@ -17,5 +17,23 @@ class CompanyDetailsController < ApplicationController
     else
       render json: { message: @curriculum.errors.full_messages }
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :email)
+  end
+
+  def company_params
+    params.require(:company).permit(:name)
+  end
+
+  def params_address
+    params[:company].require(:company_address).permit(:country, :cep, :state, :city, :hood, :street, :cel)
+  end
+
+  def params_personal
+    params[:company].require(:company_personal).permit(:cnpj, :size, :about_us)
   end
 end
