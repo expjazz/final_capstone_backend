8f14240d1a1c459616b56a72dfadb1b7edcc1f97
Expedito Andrade <expeditojazz@gmail.com>
2020-08-19 08:07:18 -0300

    remove byebug

--- app/controllers/curriculums_controller.rb
+++ app/controllers/curriculums_controller.rb
@@ -6,7 +6,6 @@ class CurriculumsController < ApplicationController
     address = CandidateAddress.create(params_address)
     personal = CandidatePersonal.create(params_personal)
     @curriculum = Curriculum.new(params_curriculum)
-    byebug
     @curriculum.candidate_personal = personal
     @curriculum.candidate_address = address
     if params[:curriculum][:jobs]
