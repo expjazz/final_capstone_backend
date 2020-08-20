bc7f0dd2e263bf746fbe42a00899ac962013598c
Expedito Andrade <expeditojazz@gmail.com>
2020-08-20 15:48:11 -0300

    add jobs.candidates

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :curriculum
-  has_many :candidates, class_name: 'CandidateJobApplication', foreign_key: 'job_id'
+  has_many :candidates_applied, class_name: 'CandidateJobApplication', foreign_key: 'job_id'
+  has_many :candidates, through: :candidates_applied, source: :candidate
 end
