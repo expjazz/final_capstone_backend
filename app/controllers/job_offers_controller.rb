985f05b577b1bac4fa7ee6a1a28df1dc29668a0e
Expedito Andrade <expeditojazz@gmail.com>
2020-08-20 08:07:17 -0300

    add  create jobOffers to backend

--- app/controllers/job_offers_controller.rb
+++ app/controllers/job_offers_controller.rb
@@ -1,7 +1,23 @@
 class JobOffersController < ApplicationController
+  before_action :require_current_user
   def create
+    @user = User.find(info_current_user[0]['user_id'])
+    @job = JobOffer.new(params_job)
+    @job.user = @user 
+    if @job.save 
+      render json: @job 
+    else  
+      render json: { message: @job.errors.full_messages}
+    end
   end
 
   def index
   end
+
+  private
+
+  def params_job 
+    params.require(:job_offer).permit(:requirement, :salary, :position)
+  end
 end
+
