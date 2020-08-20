3fddb64fcdb5caa3c42216f697e5299f21a80b0d
Expedito Andrade <expeditojazz@gmail.com>
2020-08-20 05:47:04 -0300

    fix bug in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,7 +28,11 @@ class SessionsController < Devise::SessionsController
       token = JWT.decode(cookies[:token], ENV['DEVISE_SECRET_KEY'])
       @user = User.find(token[0]['user_id'])
       if @user.profile_type == 'Candidate'
-        render json: { user: { name: @user.profile.name, gerenalInfo: @user }, curriculum: { header: @user.curriculum, pastJobs: @user.curriculum.jobs, address: @user.curriculum.candidate_address, personal: @user.curriculum.candidate_personal } }
+        if @user.curriculum
+          render json: { user: { name: @user.profile.name, gerenalInfo: @user }, curriculum: { header: @user.curriculum, pastJobs: @user.curriculum.jobs, address: @user.curriculum.candidate_address, personal: @user.curriculum.candidate_personal } }
+        else
+          render json: { user: { generalInfo: @user, name: @user.profile.name } }
+        end
       elsif @user.profile_type == 'Company'
         render json: { user: { name: @user.profile.name, generalInfo: @user }, companyInfo: { header: @user.profile.header, jobOffers: @user.job_offers, address: @user.profile.company_address, personal: @user.profile.company_personal } }
 
