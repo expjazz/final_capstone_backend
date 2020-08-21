502550c2418ca1a0a1fe2437957396ba34b6cae9
Expedito Andrade <expeditojazz@gmail.com>
2020-08-21 17:33:22 -0300

    add interview create action

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,7 +1,13 @@
 class InterviewsController < ApplicationController
   def create
+    @job = JobOffer.find(params[:job_offer_id])
+    if params[:company_id]
+      @company = Company.find(params[:company_id])
+      @user = User.find(info_current_user[0]['user_id'])
+      @candidate = Candidate.find(@user.profile_id)
+      @interview = Interview.create(candidate: @candidate, company: @company, job_offer: @job, status: 'waiting for confirmation from the company', time: params[:time])
+    end
   end
 
-  def update
-  end
+  def update; end
 end
