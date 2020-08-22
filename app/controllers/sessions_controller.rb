58e85265e7d72cee5f927127824db8c433b20cad
Expedito Andrade <expeditojazz@gmail.com>
2020-08-22 10:31:48 -0300

    fix bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < Devise::SessionsController
 
         end
       elsif @user.profile_type == 'Company'
-        render json: { user: { name: @user.profile.name, generalInfo: @user }, companyInfo: { header: @user.profile.header, jobOffers: @user.job_offers.as_json(include: { candidates: { include: { user: { include: { curriculum: { include: %i[candidate_address candidate_personal] } }, only: %i[email id] } }, only: %i[name id] } }), address: @user.profile.company_address, personal: @user.profile.company_personal } }
+        render json: { user: { name: @user.profile.name, generalInfo: @user }, companyInfo: { header: @user.profile.header, jobOffers: @user.job_offers.as_json(include: { candidates: { include: { user: { include: { curriculum: { include: %i[candidate_address candidate_personal] } }, only: %i[email id] } }, only: %i[name id] } }), address: @user.profile.company_address, personal: @user.profile.company_personal }, interviews: @user.profile.interviews.as_json(include: %i[company candidate job_offer]) }
       end
     else
       render json: { message: 'Wrong email or password' }
@@ -34,7 +34,7 @@ class SessionsController < Devise::SessionsController
           render json: { user: { generalInfo: @user, name: @user.profile.name, jobsApplied: @user.profile.jobs_applied } }
         end
       elsif @user.profile_type == 'Company'
-        render json: { user: { name: @user.profile.name, generalInfo: @user }, companyInfo: { header: @user.profile.header, jobOffers: @user.job_offers.as_json(include: { candidates: { include: { user: { include: { curriculum: { include: %i[candidate_address candidate_personal] } }, only: %i[email id] } }, only: %i[name id] } }), address: @user.profile.company_address, personal: @user.profile.company_personal } }
+        render json: { user: { name: @user.profile.name, generalInfo: @user }, companyInfo: { header: @user.profile.header, jobOffers: @user.job_offers.as_json(include: { candidates: { include: { user: { include: { curriculum: { include: %i[candidate_address candidate_personal] } }, only: %i[email id] } }, only: %i[name id] } }), address: @user.profile.company_address, personal: @user.profile.company_personal }, interviews: @user.profile.interviews.as_json(include: %i[company candidate job_offer]) }
 
       end
     else
