16210d55235a93a55f1947954b666dddef227fac
Expedito Andrade <expeditojazz@gmail.com>
2020-08-25 17:39:48 -0300

    remve bugs in migrations

--- db/migrate/20200821202222_add_time_to_interviews.rb
+++
@@ -1,5 +0,0 @@
-class AddTimeToInterviews < ActiveRecord::Migration[6.0]
-  def change
-    add_column :interviews, :time, :string
-  end
-end
--- db/migrate/20200822113012_removebugfrominterviews.rb
+++
@@ -1,5 +0,0 @@
-class Removebugfrominterviews < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :interviews
-  end
-end
